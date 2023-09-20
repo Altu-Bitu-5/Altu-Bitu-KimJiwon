b3006b19587c64cad40b3a15cf4e894177c30085
Jiwon <you@example.com>
2023-09-20 19:32:53 +0900

    [Fix] 디스크컨트롤러.py

--- 05_우선순위큐/디스크 컨트롤러.py
+++ 05_우선순위큐/디스크 컨트롤러.py
@@ -9,21 +9,23 @@ import heapq
 
 
 def solution(jobs):
-    n_jobs = len(jobs)
-    h = []
-    result, current_time, i = 0, 0, 0
-
+    answer, now, i = 0, 0, 0
     start = -1
-    while i < n_jobs:
-        for job in jobs:
-            if start < job[0] <= current_time:
-                heapq.heappush(h, [job[1], job[0]])
-        if len(h) > 0:
-            et, st = heapq.heappop(h)
-            start = current_time
-            current_time += et
-            result += current_time - st
+    heap = []
+
+    while i < len(jobs):
+        # 현재 시점에서 처리할 수 있는 작업을 heap에 저장
+        for j in jobs:
+            if start < j[0] <= now:
+                heapq.heappush(heap, [j[1], j[0]])
+
+        if len(heap) > 0:  # 처리할 작업이 있는 경우
+            cur = heapq.heappop(heap)  # 우선순위가 가장 높은 job을 수행 -> [0,3]
+            start = now
+            now += cur[0]  # 0
+            answer += now - cur[1]  # 작업 요청시간부터 종료시간까지의 시간 계산
             i += 1
-        else:
-            current_time += 1
-    return int(result / n_jobs)
+        else:  # 처리할 작업이 없는 경우 다음 시간을 넘어감
+            now += 1
+
+    return answer // len(jobs)
